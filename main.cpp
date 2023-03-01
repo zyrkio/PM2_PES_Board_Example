ef89de73c2fccc7931df68ae7755a0d178346fa5
taku <deddoame@gmail.com>
2023-03-01 09:58:19 +0100

    test 2

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 
 # define M_PI 3.14159265358979323846 // number pi, an example in case you need it
 //sd
+//tdsjfjds
 //testasdasd
 bool do_execute_main_task = false; // this variable will be toggled via the user button (blue button) and decides whether to execute the main task or not
 bool do_reset_all_once = false;    // this variable is used to reset certain variables and objects and shows how you can run a code segment only once
